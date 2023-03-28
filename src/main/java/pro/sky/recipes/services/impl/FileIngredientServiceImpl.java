905592771266c7a876843745dad683c912c13f80
Inna <serebryakovainna@mail.ru>
2023-03-28 16:52:42 +0300

    Add commit 15

--- src/main/java/pro/sky/recipes/services/impl/FileIngredientServiceImpl.java
+++ src/main/java/pro/sky/recipes/services/impl/FileIngredientServiceImpl.java
@@ -1,4 +1,64 @@
 package pro.sky.recipes.services.impl;
 
-public class FileIngredientServiceImpl {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import pro.sky.recipes.services.FileService;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class FileIngredientServiceImpl implements FileService {
+    @Value("${path.to.date.file}")
+    private String dataFilePath;
+    @Value("${name.to.date.file1}")
+    private String dataFileName;
+
+    @Override
+    public boolean saveToFile(String json) {
+        try {
+            cleanDateFile();
+            Files.writeString(Path.of(dataFilePath, dataFileName), json);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public String readFromFile() {
+        try {
+            return Files.readString(Path.of(dataFilePath, dataFileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean cleanDateFile() {
+        try {
+            Path path = Path.of(dataFilePath, dataFileName);
+            Files.deleteIfExists(path);
+            Files.createFile(path);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public Path creatTempFile(String suffix) {
+        return null;
+    }
+
+    @Override
+    public File getDataFile() {
+        return null;
+    }
 }
+

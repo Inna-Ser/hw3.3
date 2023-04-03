a9389d50a67253be7b66fd1b0ffaa1a8d9cc0fe0
Inna <serebryakovainna@mail.ru>
2023-04-03 18:49:07 +0300

    Add commit 23

--- src/main/java/pro/sky/recipes/controllers/FileController.java
+++ src/main/java/pro/sky/recipes/controllers/FileController.java
@@ -1,5 +1,9 @@
 package pro.sky.recipes.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -20,9 +24,6 @@ public class FileController {
     private FileRecipeImpl recipeFileService;
     private FileIngredientImpl ingredientFileService;
 
-    public FileController() {
-    }
-
     public FileController(FileRecipeImpl recipeFileService, FileIngredientImpl ingredientFileService) {
         this.recipeFileService = recipeFileService;
         this.ingredientFileService = ingredientFileService;
@@ -30,6 +31,22 @@ public class FileController {
 
 
     @GetMapping(value = "/export", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "download recipe file",
+            description = "we have to select a file to download"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "file has been downloaded",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json"
+                            )
+                    }
+
+            )
+    })
     public ResponseEntity<InputStreamResource> downloadDateFile() throws FileNotFoundException {
         File file = recipeFileService.getDataFile();
         if (file.exists()) {
@@ -44,6 +61,22 @@ public class FileController {
     }
 
     @PostMapping(value = "/ingredient/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(
+            summary = "upload ingredient file",
+            description = "we have to select a file to upload"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "file has been uploaded",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json"
+                            )
+                    }
+
+            )
+    })
     public ResponseEntity<Void> uploadDateFileIngredient(@RequestParam MultipartFile file) {
         ingredientFileService.cleanDateFile();
         File dateFile = ingredientFileService.getDataFile();
@@ -58,6 +91,22 @@ public class FileController {
     }
 
     @PostMapping(value = "/recipe/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(
+            summary = "upload recipe file",
+            description = "we have to select a file to upload"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "file has been uploaded",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json"
+                            )
+                    }
+
+            )
+    })
     public ResponseEntity<Void> uploadDateFileRecipe(@RequestParam MultipartFile file) {
         recipeFileService.cleanDateFile();
         File dateFile = recipeFileService.getDataFile();
--- src/main/java/pro/sky/recipes/controllers/InfoController.java
+++ src/main/java/pro/sky/recipes/controllers/InfoController.java
@@ -1,5 +1,9 @@
 package pro.sky.recipes.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +18,22 @@ public class InfoController {
         return "Application is started";
     }
 
-    @GetMapping("/info")
+    @GetMapping("src/main/java/pro.sky.recipes/controllers/info")
+    @Operation(
+            summary = "info about project"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "information displayed on the screen",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json"
+                            )
+                    }
+
+            )
+    })
     public String info() {
         return "Inna Serebriakova, Recipe App, 14.03.2023, application of recipes";
     }
